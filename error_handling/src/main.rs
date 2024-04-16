5def09eb34175a7d8cb64a4124514801ede62532
dhiraj0911 <djirajbari123@gmail.com>
2024-04-16 18:12:11 +0530

    expect, unwrap, ?, result return functions

--- error_handling/src/main.rs
+++ error_handling/src/main.rs
@@ -1,4 +1,6 @@
 use std::{fs::File, io::ErrorKind};
+use std::io;
+use std::io::Read;
 
 fn main() {
     // panic!("this is error");
@@ -8,19 +10,49 @@ fn main() {
     //     Ok(T),
     //     Err(E)
     // }
-    
+
     let f = File::open("hello.txt");
     
-    let f = match f {
+    match f {
         Ok(file) => file,
         Err(error) => match error.kind() {
             ErrorKind::NotFound => match File::create("hello.txt") {
                 Ok(fc) => fc,
-                Err(err) => panic!("this is error while creating file: {:?}", error)
+                Err(err) => panic!("this is error while creating file: {:?}", err)
             }
             other_error => {
                 panic!("Error in reading file: {:?}", other_error)
             }
-        }
+        }   
     };
+
+    // Using `unwrap` function
+
+    // let f = File::open("hello.txt").unwrap();
+    // let f = File::open("hello.txt").expect("error in reading file");
+}
+
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    // let mut f = File::open("hello.txt")?;
+
+    // // let mut f = match f{
+    // //     Ok(file) => file,
+    // //     Err(err) => Err(e)
+    // // };
+
+    // let mut s = String::new();
+
+    // // match f.read_to_string(&mut s) {
+    // //     Ok(_) => Ok(s),
+    // //     Err(e) => Err(e)
+    // // }
+    // f.
+    // Ok(s)
+
+    let mut s = String::new();
+    File::open("hello.txt")?.read_to_string(&mut s)?;
+    Ok(s)
+
+    // ? operator only works where function return Result of Options
 }
